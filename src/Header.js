10b27d4f43cc698453914a9f86f5351b9eab2698
sthakur2 <sahil.thakur16@gmail.com>
2021-03-01 23:33:29 -0500

    update header.js
    update header.js

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,18 @@ import './Header.css';
 function Header() {
     return <div className="header">
         <div className='header__left'>
-
+      {/* Avatar for logged in User */}
+      {/* Time icon */}
+        </div>
+        <div className="header__search">
+            {/* Search icon */}
+            {/* input */}
+        </div>
+        <div className="header__right">
+            {/* help icon */}
         </div>
             
     </div>;
     
 }
-export default Header
+export default Header;
