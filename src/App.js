9d1e39255d20e1fec8eb23cad44be81ec00ddb1d
sthakur2 <sahil.thakur16@gmail.com>
2021-03-30 12:49:00 -0400

    New update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
+    // BEM naming convention
+    <div className="app">
       <h1> Hello People. This is slack clone :rock </h1>
       {/* Header */}
+      <Header />
       {/* Sidebar */}
       {/* React-Router -> Chat Screen */}
-    </div>
+    </div>   
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import './Header.css';
-
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime"
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon  from '@material-ui/icons/Help';
 function Header() {
-    return <div className="header">
-        <div className='header__left'>
-      {/* Avatar for logged in User */}
-      {/* Time icon */}
+    return (
+        <div className="header">
+           <div className='header__left'>
+           <Avatar
+                className="header__avatar"
+                alt="SahilThakur"
+                src="" 
+                />    
+        <AccessTimeIcon />
+        </div> 
+        <div className='header__search'>
+        <SearchIcon />
+        <input placeholder="Search Sahil Slack" />
+        
         </div>
-        <div className="header__search">
-            {/* Search icon */}
-            {/* input */}
+        <div className='header__right'>
+         <HelpOutlineIcon />   
         </div>
-        <div className="header__right">
-            {/* help icon */}
-        </div>
-            
-    </div>;
-    
+      </div>
+    )
 }
-export default Header;
+
+export default Header
+
